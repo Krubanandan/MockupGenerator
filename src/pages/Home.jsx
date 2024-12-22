126ad078c23b45228c2f3e0e194484ab9cb9a80f
Krubanandan S <krubanandanks@gmail.com>
2024-12-23 00:32:13 +0530

    hero page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import iphoneMock from "../assets/iphoneMock.jpg";
 import iphoneMock1 from "../assets/iphonem1.jpg";
-import iphoneMock2 from "../assets/iphonem2.webp";
+import iphoneMock2 from "../assets/iphonem2.jpg";
 import akLogo from "../assets/akLogo.png";
 import akMock from "../assets/ak1.png";
 import { useNavigate } from "react-router-dom";
@@ -17,84 +17,103 @@ const Home = () => {
   return (
     <>
       <div className="mx-10 flex flex-col items-center border-gray-400">
-        <div className="text-[#414141]">
-          <h1 className="text-7xl px-10 pt-10 pb-4">Generate Mockups Easily</h1>
+        <div>
+          <h1 className="text-[#414141] text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl px-10 pt-10 pb-4">
+            Generate Mockups Easily
+          </h1>
+          <h3 className="text-[#5a5a5a] mx-auto items-center text-center sm:text-base md:text-xl lg:text-2xl xl:text-3xl">Effortlessly Create Professional Mockups with Just a Click!</h3>
         </div>
         <button
-          className="p-3 bg-gray-100 w-75 m-5  rounded-xl"
+          className="p-3 bg-gray-100 w-1/2 sm:w-1/3 md:w-1/4 lg:w-48 m-8 rounded-xl"
           onClick={() => navigate("generate")}
         >
           Make Your Mockup
         </button>
-        <div className="flex flex-row">
-          <img className="w-1/3" src={iphoneMock} alt=""></img>
-          <img className="w-1/3" src={iphoneMock1} alt="" />
-          <img className="w-1/3" src={iphoneMock2} alt="" />
+        <div className="flex flex-col sm:flex-row sm:mx-auto">
+          <img className="w-full sm:w-1/3" src={iphoneMock} alt="iPhone Mock" />
+          <img className="w-full sm:w-1/3" src={iphoneMock1} alt="iPhone Mock 1" />
+          <img className="w-full sm:w-1/3" src={iphoneMock2} alt="iPhone Mock 2" />
         </div>
       </div>
       <div>
-        <h1 className="text-5xl px-10 pt-10 pb-5 text-[#414141]">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl px-10 pt-10 pb-5 text-[#414141]">
           What are Mockups?
         </h1>
-        <p className="w-3/4 mx-auto text-center text-2xl">
+        <p className="w-full sm:w-3/4 mx-auto text-center text-lg sm:text-xl md:text-2xl">
           Mockups are visual representations or prototypes of a product,
           typically used in design and development processes to showcase how a
           final product will look and function. They help in communicating
           design ideas, gathering feedback, and aligning the team on the
           project's direction.
         </p>
-        <div className="flex flex-row items-center justify-center gap-10 p-9">
-          <div className="flex flex-col items-center justify-center">
-            <img src={akLogo} className="max-w-96" alt="" />
+        <div className="flex flex-col sm:flex-row sm:gap-10 items-center justify-center  p-9">
+          <div className="flex flex-col items-center justify-center mb-5 sm:mb-0">
+            <img src={akLogo} className="max-w-full sm:max-w-96" alt="Restaurant Logo" />
             <p className="text-xl p-3">Restaurant Logo</p>
           </div>
           <div className="flex flex-col items-center justify-center">
-            <img src={akMock} className="max-w-96" alt="" />
+            <img src={akMock} className="max-w-full sm:max-w-96" alt="Restaurant Sign MockUp" />
             <p className="text-xl p-3">Restaurant Sign MockUp</p>
           </div>
         </div>
       </div>
 
       <div>
-        <h1 className="text-4xl px-10 pt-5 pb-5 text-[#414141]">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl px-10 pt-5 pb-5 text-[#414141]">
           Benefits of Mockups
         </h1>
-        <p className="w-3/4 mx-auto text-center justify-start text-2xl">
+        <p className="w-3/4 sm:w-3/4 mx-auto text-center text-lg sm:text-xl md:text-2xl">
           Mockups elevate your design presentations, save time on revisions,
           boost client satisfaction, simplify product marketing, and create
           stunning visuals for ads instantly.
         </p>
         <img
           src={mockExample}
-          className="w-1/2 items-center mx-auto p-8"
-          alt=""
+          className="w-full sm:w-1/2 mx-auto p-8"
+          alt="Mockup Example"
         />
-        <p className="w-3/4 mx-auto text-start justify-start text-2xl py-3 font-medium">
+        <p className="w-3/4 sm:w-3/4 mx-auto text-start text-lg sm:text-xl py-3 font-medium">
           Portfolio and Social Media Presentation
         </p>
-        <p className="w-3/4 mx-auto text-lg justify-center text-pretty">Mockups are a key asset for presenting your design work in a polished and professional manner. Instead of just showcasing raw designs, mockups allow you to place them in realistic settings, such as on devices or products, making them more visually engaging. Whether you’re sharing your work on your portfolio or social media, mockups provide a sleek, eye-catching presentation that helps your designs stand out. This makes it easier for potential clients or followers to appreciate the full impact of your work.</p>
-        <img src={b1} alt="" className="w-1/2 mx-auto p-5"/>
-        <p className="w-3/4 mx-auto text-start justify-start text-2xl py-3 font-medium">
-        Client Presentations
+        <p className="w-4/5 sm:w-3/4 mx-auto text-md sm:text-lg">
+          Mockups are a key asset for presenting your design work in a polished and professional manner.
+          Instead of just showcasing raw designs, mockups allow you to place them in realistic settings, 
+          such as on devices or products, making them more visually engaging.
         </p>
-        <p className="w-3/4 mx-auto text-lg justify-center text-pretty"> In client presentations, mockups serve as a powerful tool for conveying your design vision in a tangible way. Clients are often more likely to approve designs when they can visualize how the final product will look in a real-world context, and mockups provide just that. By inserting your designs into mockups, you can showcase them in environments like mobile screens, posters, or packaging, making it easier for clients to grasp the design’s functionality and appeal.</p>
-        <img src={b2} alt="" className="w-1/2 mx-auto p-5"/>
-        <p className="w-3/4 mx-auto text-start justify-start text-2xl py-3 font-medium">
-        Speed Up Design Process
+        <img src={b1} alt="b1" className="w-full sm:w-1/2 mx-auto p-5" />
+        <p className="w-3/4 sm:w-3/4 mx-auto text-start text-lg sm:text-xl py-3 font-medium">
+          Client Presentations
         </p>
-        <p className="w-3/4 mx-auto text-lg justify-center text-pretty"> Mockups can significantly speed up the design process by helping designers catch errors early. When designs are inserted into mockups, it becomes easier to spot issues like incorrect aspect ratios, font sizes, or color mismatches that might not be apparent in a design file. This allows for quick adjustments before moving on to final production, preventing costly revisions later.</p>
-        <img src={b3} alt="" className="w-1/2 mx-auto p-5"/>
-        <p className="w-3/4 mx-auto text-start justify-start text-2xl py-3 font-medium">
-        E-commerce
+        <p className="w-4/5 sm:w-3/4 mx-auto text-md sm:text-lg">
+          In client presentations, mockups serve as a powerful tool for conveying your design vision in a tangible way.
         </p>
-        <p className="w-3/4 mx-auto text-lg justify-center text-pretty">In e-commerce, mockups are invaluable for displaying products online before they are physically produced. With mockups, you can create high-quality visuals of your product packaging, clothing designs, or even digital goods, and display them on your website or social media platforms. This allows you to market and sell products before they are manufactured, giving you a head start on generating interest and revenue.</p>
-        <img src={b4} alt="" className="w-1/2 mx-auto p-5"/>
-        <p className="w-3/4 mx-auto text-start justify-start text-2xl py-3 font-medium">
-        Advertising
+        <img src={b2} alt="b2" className="w-full sm:w-1/2 mx-auto p-5" />
+        <p className="w-3/4 sm:w-3/4 mx-auto text-start text-lg sm:text-xl py-3 font-medium">
+          Speed Up Design Process
         </p>
-        <p className="w-3/4 mx-auto text-lg justify-center text-pretty">Mockups play a crucial role in creating high-quality advertising materials with minimal effort. Once a design is finalized, it can be easily inserted into a mockup to create professional visuals for use in ads, social media posts, or promotional materials. This allows you to quickly produce eye-catching graphics that are ready for deployment without the need for complex photography or additional editing.</p>
-        <img src={b5} alt="" className="w-1/2 mx-auto p-5"/>
+        <p className="w-4/5 sm:w-3/4 mx-auto text-md sm:text-lg">
+          Mockups can significantly speed up the design process by helping designers catch errors early.
+        </p>
+        <img src={b3} alt="b3" className="w-full sm:w-1/2 mx-auto p-5" />
+        <p className="w-3/4 sm:w-3/4 mx-auto text-start text-lg sm:text-xl py-3 font-medium">
+          E-commerce
+        </p>
+        <p className="w-4/5 sm:w-3/4 mx-auto text-md sm:text-lg">
+          In e-commerce, mockups are invaluable for displaying products online before they are physically produced.
+        </p>
+        <img src={b4} alt="b4" className="w-full sm:w-1/2 mx-auto p-5" />
+        <p className="w-3/4 sm:w-3/4 mx-auto text-start text-lg sm:text-xl py-3 font-medium">
+          Advertising
+        </p>
+        <p className="w-4/5 sm:w-3/4 mx-auto text-md sm:text-lg">
+          Mockups play a crucial role in creating high-quality advertising materials with minimal effort.
+        </p>
+        <img src={b5} alt="b5" className="w-full sm:w-1/2 mx-auto p-5" />
       </div>
+      <div class="bg-gray-300 xs:bg-red-500 sm:bg-blue-500 md:bg-green-500 lg:bg-yellow-500 xl:bg-purple-500 2xl:bg-pink-500 3xl:bg-teal-500">
+  Responsive Background Colors
+</div>
+
     </>
   );
 };
