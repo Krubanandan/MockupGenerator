f11105019ebd242cdb9b5a4aeb9c08c5dc30b67b
Krubanandan S <krubanandanks@gmail.com>
2024-12-22 19:36:06 +0530

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Ensure the base path is set correctly
+  build: {
+    outDir: 'dist', // Default output folder, should match your deployment settings
+  },
+  server: {
+    open: true,
+  },
+});
